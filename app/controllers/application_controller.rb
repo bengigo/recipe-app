bcf742b8b4b74f85d1adb8d8e7b0b6a1977669d6
Bengi Genç Bıyık <bengi@Bengi-MacBook-Air.local>
2022-11-16 19:31:00 +0300

    Fix sign up bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name bio photo posts_counter])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name bio photo posts_counter])
-  end
-
-  def authorize_request
-    header = request.headers['Auth']
-    header = header.split.last if header
-    begin
-      @decoded = JsonWebToken.decode(header)
-      p @decoded
-      @current_user = User.find(@decoded['id'])
-    rescue ActiveRecord::RecordNotFound => e
-      render json: { errors: e.message }, status: :unauthorized
-    rescue JWT::DecodeError => e
-      render json: { error: e.message }, status: :unauthorized
-    end
-  end
 end
