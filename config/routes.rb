4be19811fd40644c48e8389133cd152c13d9f98b
Bengi Genç Bıyık <bengi@Bengi-MacBook-Air.local>
2022-11-16 19:40:24 +0300

    End user session

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   # root 'pages#index'
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root 'recipes#index'
+
 end
