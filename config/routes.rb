5c3122f7b1c0c2b14d14bef719c2b6c29e2f60e4
Bengi Genç Bıyık <bengi@Bengi-MacBook-Air.local>
2022-11-17 22:17:28 +0300

    Lead user to food form

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,12 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-
   root "recipes#public"
-
-
-
   resources :users do
-    resources :recipes
+    resources :recipes, except: :update do
+      resources :recipe_foods
+    end
   end
+  resources :foods, except: :update
 
 end
