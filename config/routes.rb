6471041690c72df70d8d82bdcb9c54fa692e0bb3
shaqayq <shaqayq.darwazi@gmail.com>
2022-11-18 13:55:45 +0430

    create recpice detail page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'recipes/index', to: 'recipes#index'
   get 'recipes/public', to: 'recipes#public'
+  post '/recipes/show/:id', to: 'recipes#show'
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
