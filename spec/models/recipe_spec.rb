7bc71ddd6de0c07fabb6f224d943269270a73057
Bengi Genç Bıyık <genc.bengi@gmail.com>
2022-11-19 00:13:10 +0300

    Add recipe specs

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @recipe = Recipe.new(name: "Recipe 1", preparation_time: 10, cooking_time: 20, description: "This is a recipe", public: true)
+  end
+  it "has a name" do    
+    expect(@recipe.name).to eq("Recipe 1")
+  end
+  it "is prepared in 10 minutes" do
+    expect(@recipe.preparation_time).to eq("10")
+  end
+  it "is cooked in 20 minutes" do
+    expect(@recipe.cooking_time).to eq("20")
+  end
+  it "has a description" do
+    expect(@recipe.description).to eq("This is a recipe")
+  end
 end
