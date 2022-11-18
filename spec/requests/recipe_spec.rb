9e3a3b772e9b82f6f0d6b19ecd4b1d5994aa2350
Bengi Genç Bıyık <genc.bengi@gmail.com>
2022-11-18 23:47:05 +0300

    Add recipe specs

--- spec/requests/recipe_spec.rb
+++ spec/requests/recipe_spec.rb
@@ -2,6 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'Recipes', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before do
+      @user = User.create(email: 'user@mail.com', password: "123456")
+      @recipe = @user.recipes.create(name: 'Recipe 1', preparation_time: 20, cooking_time: 30, description: 'Recipe 1 description', public: true)
+    end
+    it 'returns http success' do
+      get(user_recipes_path(@user))
+      expect(response).to have_http_status(:ok)
+    end
+    it 'loads all of the recipes' do
+      get(user_recipes_path(@user))
+      expect(response.body).to include(@recipe.name)
+    end
   end
 end
