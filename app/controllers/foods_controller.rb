af7f5ba2e9628a8e21c764e497fb3cd103d5cca8
shaqayq <shaqayq.darwazi@gmail.com>
2022-11-19 12:42:08 +0430

    Design Shopping list Page

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,7 @@
 class FoodsController < ApplicationController
-       def index
+    before_action :authenticate_user!
+    
+    def index
     @user=current_user
     @foods=@user.foods.includes(:user)
    end
