67f16ce58521f35915c5b3365dc5e76081978ecd
Bengi Genç Bıyık <genc.bengi@gmail.com>
2022-11-19 18:20:10 +0300

    Fix food delete issue

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -9,6 +9,11 @@ class RecipeFoodsController < ApplicationController
     @recipe_food = RecipeFood.new
   end
 
+  def destroy
+    @recipe_food.destroy
+    redirect_to user_recipe_path(current_user, @recipe_food.recipe)
+  end
+
   def create
     @recipe = Recipe.find(params[:recipe_id])
     @recipe_food = @recipe.recipe_foods.new(recipe_food_params)
@@ -19,6 +24,11 @@ class RecipeFoodsController < ApplicationController
     end
 
   end
+
+  def set_recipe_food
+    @recipe_food = RecipeFood.find(params[:id])
+  end
+
 
   def recipe_food_params
     params.require(:recipe_food).permit(:quantity, :food_id)
