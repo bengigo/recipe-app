2bd3076a03d730bba371f70d92cfbe029b87846b
shaqayq <shaqayq.darwazi@gmail.com>
2022-11-19 13:00:13 +0430

    Design login page

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,5 @@
 class Food < ApplicationRecord
 
-
   belongs_to :user , foreign_key: 'user_id' , class_name: 'User'
   has_many :recipe_foods , dependent: :delete_all
   has_many :recipes, through: :recipe_foods
